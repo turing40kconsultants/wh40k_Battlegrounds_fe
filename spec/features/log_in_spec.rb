4f1f88941929c7e683f59d27c89fa19fa7f17dd8
lance butler <lancej2004@gmail.com>
2024-07-24 01:59:54 -0600

    log in form

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Log In" do
+  before(:each) do
+    @user1 = User.create!(email: "test@test.com", password: "123456", password_digest: "123456")
+  end
+
+  it "can log in to an account successfully" do
+    visit "/login"
+
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_button("Log In")
+
+    fill_in :email, with: "test@test.com"
+    fill_in :password, with: "123456"
+    save_and_open_page
+    click_on "Log In"
+    expect(current_path).to eq("/dashboard")
+  end
+end
