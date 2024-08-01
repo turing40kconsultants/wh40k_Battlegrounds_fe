d0c4f1bc4419997489ab148730bf1fc22cefcd15
Clyde <clyde.autin@gmail.com>
2024-08-01 10:58:42 -0700

    players choices of attacker and defender are now stored in cookies allowing us to play different factions against each other

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,22 @@
 class DashboardController < ApplicationController
   def index
-    redirect_to root_path if current_user.nil?
+    # redirect_to root_path if current_user.nil?
+    # require 'pry'; binding.pry
 
-    if params[:player]
-      @player = JSON.parse(params[:player], symbolize_names: true)
+    if params[:attacker]
+      # require 'pry'; binding.pry
+      @attacker = JSON.parse(params[:attacker], symbolize_names: true)
+      cookies[:attacker] = params[:attacker]
+    else
+      @attacker = JSON.parse(cookies[:attacker], symbolize_names: true) if cookies[:attacker]
     end
 
-    if params[:opponent]
-      @opponent = JSON.parse(params[:opponent], symbolize_names: true)
+    if params[:defender]
+      @defender = JSON.parse(params[:defender], symbolize_names: true)
+      cookies[:defender] = params[:defender]
+      require 'pry'; binding.pry
+    else
+      @defender = JSON.parse(cookies[:defender], symbolize_names: true) if cookies[:defender]
     end
   end
 end
